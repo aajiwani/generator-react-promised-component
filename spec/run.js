680c03fbe878174f99a22b45eee8d543fbb797d8
Amir Ali Jiwani <amirali.jiwani89@gmail.com>
2017-08-06 10:21:32 +0800

    Adds jasmine support for testing the generator

---
+++ spec/run.js
@@ -0,0 +1,4 @@
+import Jasmine from 'jasmine';
+var jasmine = new Jasmine();
+jasmine.loadConfigFile('spec/support/jasmine.json');
+jasmine.execute();
---
+++ spec/tests/basicSpec.js
@@ -0,0 +1,6 @@
+describe("A barebones test", function()
+{
+  it("should expect true out of true", () => {
+    expect(true).toBe(true);
+  });
+});
